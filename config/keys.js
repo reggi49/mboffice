933838b790a30eda4e8fb5096acc560e3f429ff3
Regi <regi@Regis-MacBook-Pro.local>
2018-08-09 15:57:14 +0700

    fix typo

--- config/keys.js
+++ config/keys.js
@@ -3,7 +3,7 @@ if (process.env.NODE_ENV === 'production'){
     googleClientID  : process.env.GOOGLE_CLIENT_ID,
     mongoClientSecret : process.env.GOOGLE_CLIENT_SECRET,
     mongoURI  : process.env.MONGO_URI,
-    cookieKey : prosess.env.COOKIE_KEY
+    cookieKey : process.env.COOKIE_KEY
   };
 }else{
   module.exports = require('./dev');
