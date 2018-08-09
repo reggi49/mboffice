e94e40bbd2fdb8944dcedc0f7a03dc4edfeefe75
Regi <regi@Regis-MacBook-Pro.local>
2018-08-09 16:38:07 +0700

    fix connection mongo

--- config/keys.js
+++ config/keys.js
@@ -1,11 +1,5 @@
 if (process.env.NODE_ENV === 'production'){
-  module.exports = {
-    googleClientID  : process.env.GOOGLE_CLIENT_ID,
-    mongoClientSecret : process.env.GOOGLE_CLIENT_SECRET,
-    mongoURI  : process.env.MONGO_URI,
-    cookieKey : process.env.COOKIE_KEY
-  };
-  console.log('production xxx');
+  module.exports = { googleClientID: process.env.GOOGLE_CLIENT_ID, googleClientSecret: process.env.GOOGLE_CLIENT_SECRET, mongoURI: process.env.MONGO_URI, cookieKey: process.env.COOKIE_KEY };
 }else{
   module.exports = require('./dev');
 }
